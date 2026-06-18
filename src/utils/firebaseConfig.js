d74b535dcf07ddafee867143aea410f9c5e2f911
COIRO AGUSTIN FRANCO <95722600+AFCoiro@users.noreply.github.com>
2026-06-18 22:39:24 +0000

    arreglos de firebase

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6YERkfenGwdKFGPLezLmfHDljjh65MUQ",
-  authDomain: "afc-app-2a82e.firebaseapp.com",
-  projectId: "afc-app-2a82e",
-  storageBucket: "afc-app-2a82e.appspot.com",
-  messagingSenderId: "398695986883",
-  appId: "1:398695986883:web:37b01027c30fcced459476"
+  apiKey: "AIzaSyA93GcMhwsl-iyyfC5-ilOQmRRPjqrreB0",
+  authDomain: "tdt-afc.firebaseapp.com",
+  projectId: "tdt-afc",
+  storageBucket: "tdt-afc.firebasestorage.app",
+  messagingSenderId: "728228271537",
+  appId: "1:728228271537:web:849d8087045a71d0a89990"
 };
 
 // Initialize Firebase
